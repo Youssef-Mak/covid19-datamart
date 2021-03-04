1acb0f78383ba7a54fe9b33da9b47349848a77bd
Youssef-Mak <youssef.mak10@gmail.com>
2021-03-03 19:58:08 -0500

    Added fact table design, added phu dimension copy

--- src/database/create_tables.sql
+++ src/database/create_tables.sql
@@ -3,70 +3,97 @@ SET SCHEMA 'Covid19DataMart';
 -- Create Types
 
 DO $$
-BEGIN
+  BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'season') THEN
-        CREATE TYPE season AS ENUM (
-            'summer',
-            'fall',
-            'winter',
-            'spring'
-        );
-    END IF;
+      CREATE TYPE season AS ENUM (
+        'summer',
+        'fall',
+        'winter',
+        'spring'
+      );
+END IF;
 END$$;
 
 DO $$
-BEGIN
+  BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'gender') THEN
-        CREATE TYPE gender AS ENUM (
-            'male',
-            'female',
-            'other'
-        );
-    END IF;
+      CREATE TYPE gender AS ENUM (
+        'male',
+        'female',
+        'other'
+      );
+END IF;
 END$$;
 
 -- Dimensional Tables
 
 CREATE TABLE IF NOT EXISTS date_dimension (
-    date_dim_key serial not null,
-    full_date date,
-    day int,
-    month int,
-    year int,
-    day_of_year int, 
-    week_of_year int,
-    weekday int,
-    is_weekend boolean,
-    season season,
-    is_month_start boolean,
-    is_month_end boolean,
-    is_year_start boolean,
-    is_year_end boolean,
-    is_can_holiday boolean,
-    can_holiday_name varchar,
-    is_us_holiday boolean,
-    us_holiday_name varchar,
-    primary key (date_dim_key)
+  date_dim_key serial not null,
+  full_date date,
+  day int,
+  month int,
+  year int,
+  day_of_year int,
+  week_of_year int,
+  weekday int,
+  is_weekend boolean,
+  season season,
+  is_month_start boolean,
+  is_month_end boolean,
+  is_year_start boolean,
+  is_year_end boolean,
+  is_can_holiday boolean,
+  can_holiday_name varchar,
+  is_us_holiday boolean,
+  us_holiday_name varchar,
+  primary key (date_dim_key)
 );
 
 CREATE TABLE IF NOT EXISTS patient_dimension (
-    patient_dim_key serial not null,
-    gender gender,
-    age_group varchar(4),
-    acquisition_group varchar,
-    outbreak_related boolean,
-    primary key (patient_dim_key)
+  patient_dim_key serial not null,
+  gender gender,
+  age_group varchar(4),
+  acquisition_group varchar,
+  outbreak_related boolean,
+  primary key (patient_dim_key)
 );
 
 CREATE TABLE IF NOT EXISTS phu_dimension (
-    phu_dim_key serial not null,
-    phu_name varchar,
-    address varchar,
-    city varchar,
-    postal_code varchar,
-    province varchar,
-    url varchar,
-    latitude float,
-    longitude float,
-    primary key (phu_dim_key)
+  phu_dim_key serial not null,
+  phu_name varchar,
+  address varchar,
+  city varchar,
+  postal_code varchar,
+  province varchar,
+  url varchar,
+  latitude float,
+  longitude float,
+  primary key (phu_dim_key)
 );
+
+-- Fact table
+
+create table covid19_tracking_fact (
+  onset_date_dim_key int,
+  reported_date_dim_key int,
+  test_date_dim_key int,
+  specimen_date_dim_key int,
+  patient_dim_key int,
+  phu_dim_key int,
+  weather_dim_key int,
+  special_measures_dim_key int,
+  mobility_dim_key int,
+  resolved boolean,
+  un_resolved boolean,
+  fatal boolean,
+  primary key (onset_date_dim_key, reported_date_dim_key, test_date_dim_key, specimen_date_dim_key, patient_dim_key, phu_dim_key, weather_dim_key, special_measures_dim_key, mobility_dim_key),
+  foreign key (onset_date_dim_key) references date_dimension(date_dim_key),
+  foreign key (reported_date_dim_key) references date_dimension(date_dim_key),
+  foreign key (test_date_dim_key) references date_dimension(date_dim_key),
+  foreign key (specimen_date_dim_key) references date_dimension(date_dim_key),
+  foreign key (phu_dim_key) references phu_dimension(phu_dim_key),
+  foreign key (weather_dim_key) references weather_dimension(weather_dim_key),
+  foreign key (special_measures_dim_key) references special_measures_dimension(special_measures_dim_key),
+  foreign key (mobility_dim_key) references mobility_dimension(mobility_dim_key),
+);
+
--- src/database/load_data.sql
+++ src/database/load_data.sql
@@ -5,3 +5,5 @@ SET SCHEMA 'Covid19DataMart';
 \copy date_dimension(full_date, day, month, year, day_of_year, week_of_year, weekday, is_weekend, quarter, is_month_start, is_month_end, is_year_start, is_year_end, is_can_holiday, can_holiday_name, is_us_holiday, us_holiday_name, date_dim_key) from '../../data/dimensions/date_dimension.csv' delimiter ',' csv header;
 
 \copy patient_dimension(gender, age_group, acquisition_group, outbreak_related, patient_dim_key) from '../../data/dimensions/patient_dimension.csv' delimiter ',' csv header;
+
+\copy phu_dimension(phu_name, address, city, postal_code, province, url, latitude, latitude, phu_dim_key) from '../../data/dimensions/phu_dimension.csv' delimiter ',' csv header;
