3703540833645f9b339d0479f1b6fa398553f0f6
BillalZazai <bzaza080@uottawa.ca>
2021-04-14 21:28:27 -0400

    part b

--- src/mining/classification.py
+++ src/mining/classification.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import time
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RandomizedSearchCV
@@ -9,6 +10,9 @@ from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import metrics
+
 
 '''
 All the preprocessed data is nicely formatted in a csv file. It is located in data/preprocessed_data/mined_data.csv. 
@@ -29,71 +33,58 @@ mined_data_csv = os.path.join(
 mined_data_df = pd.read_csv(mined_data_csv)
 
 # We want to predict fatal cases
-labels = np.array(mined_data_df['fatal'])
-features = mined_data_df.drop('fatal', axis=1)
-feature_list = list(mined_data_df.columns)
-features = np.array(features)
+labels = np.array(mined_data_df[['resolved','unresolved','fatal']])
+
+
+
+
+features = mined_data_df.drop(['fatal','unresolved','resolved'], axis=1)#drops fatal, unresolved and resolved column
+feature_list = list(mined_data_df.columns)#list of columns in mined_data.csv
+features = np.array(features)#contains data of each row in csv from features, therefore fatal column is dropped
 
 train_features, test_features, train_labels, test_labels = train_test_split(
     features, labels, test_size=0.25, random_state=42)
-
 
 def gen_random_forest():
     '''
     Source: https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
     '''
+    print ('\nRandom Forest Algorithm')
+    start = time.perf_counter ()
     rf_clf = RandomForestClassifier(n_jobs=-1)
-
-    params_dist = {
-        'n_estimators': np.arange(100, 1500, 100),
-        'max_depth': [None, 2, 4, 7, 10, 20],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 3, 5, 10]
-    }
-
-    CV_rf_clf = RandomizedSearchCV(rf_clf, params_dist, cv= 5, n_iter=20, scoring='f1')
-    CV_rf_clf.fit(train_features, train_labels)
-
-    rf_pred = CV_rf_clf.best_estimator_.predict(test_features)
-    print(classification_report(test_labels, rf_pred))
+    rf_clf.fit (train_features, train_labels)
+    rf_pred=rf_clf.predict(test_features)
+    print(classification_report(test_labels, rf_pred ))
+    end = time.perf_counter ()
+    print ("Time: ", end-start)
 
 
 def gen_decision_tree():
+    print ('\nDecision Tree Algorithm')
+    start = time.perf_counter ()
     dt_clf = DecisionTreeClassifier()
-
-    params_dist = {
-        'max_depth': [None, 2, 4, 7, 10, 20],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 3, 5, 10]
-    }
-
-    CV_dt_clf = RandomizedSearchCV(dt_clf, params_dist, cv= 5)
-    CV_dt_clf.fit(train_features, train_labels)
-
-    dt_pred = CV_dt_clf.best_estimator_.predict(test_features)
+    dt_clf.fit (train_features, train_labels)
+    dt_pred = dt_clf.predict (test_features)
     print(classification_report(test_labels, dt_pred))
+    end = time.perf_counter ()
+    print ("Time: ", end-start)
 
 
-def gen_ada_boost():
-    ada_boost_clf = AdaBoostClassifier()
-
-    params_dist = {
-        'n_estimators': np.arange(50, 1500, 50),
-        'learning_rate': [1e-4, 1e-3, 1e-2, 1e-1, 1, 10],
-        'algorithm': ['SAMME', 'SAMME.R']
-    }
-
-    CV_ada_boost_clf = RandomizedSearchCV(ada_boost_clf, params_dist, cv= 5)
-    CV_ada_boost_clf.fit(train_features, train_labels)
-
-    ada_boost_pred = CV_ada_boost_clf.best_estimator_.predict(test_features)
-    print(classification_report(test_labels, ada_boost_pred))
+def gen_gradient_boost():
+    print ('\nGradient Boost Algorithm')
+    start = time.perf_counter ()
+    gradient_booster = GradientBoostingClassifier ()
+    # gradient_booster.fit (train_features, train_labels)
+    # gradient_booster_pred = gradient_booster.predict (test_features)
+    # print(classification_report(test_labels, gradient_booster_pred))
+    end = time.perf_counter ()
+    print ("Time: ", end-start)
 
 
 def main():
     gen_random_forest()
     gen_decision_tree()
-    # gen_ada_boost() #TODO: NOT WORKING
+    gen_gradient_boost() #TODO: NOT WORKING
 
 
 if __name__ == "__main__":
